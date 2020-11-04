3c6cc5a5ca9b2745a8495fab399bc753b8ae32b0
sidharth582 <73808133+sidharth582@users.noreply.github.com>
2020-11-04 20:24:04 +0530

    Update Q6.java

--- Q6.java
+++ Q6.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Q6 {
 	public static void findprime(int pos) {
 		int count=0,n=1,i;
-		while(count<pos) {
+		while(count<=pos) {
 			n=n+1;
 			for(i=2;i<=n;i++) {
 				if(n%i==0) {
