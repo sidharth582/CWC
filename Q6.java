d9d3115a12125f7734abe50379a19c84a06e95c4
sidharth582 <73808133+sidharth582@users.noreply.github.com>
2020-11-04 13:14:54 +0530

    Create Q6.java

---
+++ Q6.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+public class Q6 {
+	public static void findprime(int pos) {
+		int count=0,n=1,i;
+		while(count<pos) {
+			n=n+1;
+			for(i=2;i<=n;i++) {
+				if(n%i==0) {
+					break;
+				}
+			}if(i==n) {
+				count++;
+			}}
+		System.out.println(n);	
+	}
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int pos=10001;
+		findprime(pos);
+	}
+}
